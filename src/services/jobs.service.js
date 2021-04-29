256cefe05c24af7bcf5826bf2ffb8dc621cd30c8
Daniela <dany0913@hotmail.com>
2021-04-29 18:51:18 -0500

    still in progress the endpoint, env variable added

--- src/services/jobs.service.js
+++ src/services/jobs.service.js
@@ -1,6 +1,8 @@
+const API_URL_BASE = process.env.REACT_APP_API_URL;
+
 const getAllJobs = async () => {
   try {
-    const resp = await fetch("http://localhost:3001/api/jobs");
+    const resp = await fetch(`${API_URL_BASE}/api/jobs`);
     const jobs = await resp.json();
     return jobs;
   } catch (error) {
@@ -10,7 +12,7 @@ const getAllJobs = async () => {
 
 const getSingleJob = async (id) => {
   try {
-    const resp = await fetch(`http://localhost:3001/api/jobs/${id}`);
+    const resp = await fetch(`${API_URL_BASE}/api/jobs/${id}`);
     const jobs = await resp.json();
     return jobs;
   } catch (error) {
