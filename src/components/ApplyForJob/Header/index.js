ab3b26df109c9c62ada2a5a06b501746920899b2
Daniela <dany0913@hotmail.com>
2021-05-06 18:12:15 -0500

    WIP - Apply for job function in process,the job is charged

--- src/components/ApplyForJob/Header/index.js
+++ src/components/ApplyForJob/Header/index.js
@@ -1,37 +1,59 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getSingleJob } from "../../../services/jobs.service";
 
-const Header = () => (
-  <header className="page-header bg-img size-lg" style={{ backgroundImage: 'url(/img/bg-banner1.jpg)' }}>
-    <div className="container no-shadow">
-      <h1 className="text-center">Apply for the job</h1>
-      <p className="lead text-center">
-        Apply with your online resume, create new resume for the job,
-        or make a custom application.
-      </p>
+const Header = () => {
+  const { id } = useParams();
+  let [job, setJob] = useState({ image: "" });
+  console.log("Estamos en FORM", job);
 
-      <hr />
-      <a className="item-block item-block-flat" href="job-detail.html">
-        <header>
-          <img src="/img/logo-google.jpg" alt="Logo" />
-          <div className="hgroup">
-            <h4>Senior front-end developer</h4>
-            <h5>Google</h5>
+  const getJob = async () => {
+    const selectedJob = await getSingleJob(id);
+    console.log(selectedJob);
+    setJob(selectedJob);
+  };
+
+  useEffect(() => {
+    getJob();
+  }, [id]);
+
+  // const handleApplyNow = () => {};
+  return (
+    <header
+      className="page-header bg-img size-lg"
+      style={{ backgroundImage: "url(/img/bg-banner1.jpg)" }}
+    >
+      <div className="container no-shadow">
+        <h1 className="text-center">Apply for the job</h1>
+        <p className="lead text-center">
+          Apply with your online resume, create new resume for the job, or make
+          a custom application.
+        </p>
+
+        <hr />
+        <a className="item-block item-block-flat" href="job-detail.html">
+          <header>
+            <img src={job.image} alt={job.company} />
+            <div className="hgroup">
+              <h4>{job.title}</h4>
+              <h5>{job.company}</h5>
+            </div>
+            <div className="header-meta">
+              <span className="location">{job.location}</span>
+              <time>{job.ago}</time>
+            </div>
+          </header>
+        </a>
+
+        <div className="button-group">
+          <div className="action-buttons">
+            <button className="btn btn-primary">Apply now</button>
           </div>
-          <div className="header-meta">
-            <span className="location">Menlo park, CA</span>
-            <time>34 min ago</time>
-          </div>
-        </header>
-      </a>
-
-      <div className="button-group">
-        <div className="action-buttons">
-          <a className="btn btn-primary" href="#sec-resume">Apply now</a>
         </div>
       </div>
-
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
--- src/services/jobs.service.js
+++ src/services/jobs.service.js
@@ -13,12 +13,14 @@ const getAllJobs = async () => {
 const getSingleJob = async (id) => {
   try {
     const resp = await fetch(`${API_URL_BASE}/api/jobs/${id}`);
-    const jobs = await resp.json();
-    return jobs;
+    const job = await resp.json();
+    console.log("Estamos en getSingleJob ", job);
+    return job;
   } catch (error) {
     throw Error(error);
   }
 };
+
 const createJob = async (job) => {
   try {
     return job;
@@ -26,5 +28,22 @@ const createJob = async (job) => {
     throw Error(error);
   }
 };
+
+// const addCandidateToJob = async (id) => {
+// try {
+//   const resp = await fetch(`${API_URL_BASE}/api/jobs/${id}`);
+//   const payload = {
+//     method: "PUT",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ user }),
+//   };
+//   const resp = await fetch(`${API_URL_BASE}/api/users`, payload);
+//   const newUser = await resp.json();
+//   console.log(newUser);
+//   return newUser;
+// } catch (error) {
+//   throw Error(error);
+// }
+// };
 
 export { getAllJobs, getSingleJob, createJob };
